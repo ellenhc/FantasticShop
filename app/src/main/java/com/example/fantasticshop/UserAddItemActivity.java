c91d394f21c5ecd0d21ba1cf3056c68f68b6e488
michaelparkernorton <michaelparkernorton.va@gmail.com>
2021-11-23 13:48:58 -0300

    add item view

--- app/src/main/java/com/example/fantasticshop/UserAddItemActivity.java
+++ app/src/main/java/com/example/fantasticshop/UserAddItemActivity.java
@@ -1,2 +1,14 @@
-package com.example.fantasticshop;public class UserAddItemActivity {
+package com.example.fantasticshop;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserAddItemActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_add_item);
+    }
 }
